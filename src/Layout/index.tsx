62aa3b6f143d999b1680e51c2f84a7615a512ccf
kimtaegwan <ab154@naver.com>
2021-11-16 22:51:31 +0900

    Edit Layout directory

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -1,8 +1,14 @@
 import React, { FC } from "react";
+import Header from "./Header";
 import { Wrap } from "./styles";
 
 const Layout: FC = ({ children }) => {
-  return <Wrap>{children}</Wrap>;
+  return (
+    <Wrap>
+      <Header />
+      {children}
+    </Wrap>
+  );
 };
 
 export default Layout;
--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Layout from "../../layout/Layout";
-import MainContainer from "../../container/MainContainer";
+import Layout from "Layout";
+import MainContainer from "container/MainContainer";
 
 const MainPage = () => {
   return (
